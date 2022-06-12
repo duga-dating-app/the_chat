25bad32ae015642b02ba4c9ff1d600987b924bdf
Antonija Simic <antonija1023@gmail.com>
2022-06-12 20:33:54 +0200

    implement search users by email

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :images
   devise_for :users
-  resources :users, :only =>[:show]
+  resources :users, :only =>[:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "users#index"
 end
