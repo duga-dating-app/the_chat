1e49514945d3554aca1ea2686a1087440c2f3dc1
Antonija Simic <antonija1023@gmail.com>
2022-06-12 14:55:38 +0200

    add image to user model; show image in users index page; add tailwind

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     # devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     # devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :age, :city, :sexuality, :location, :gender])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :image, :age, :city, :sexuality, :location, :gender])
   end
 end
